26fe5de3650afa42ec08be3bb1ef380b8a1db2a4
Solange Umuhire Gasengayire <sgasengayire@gmail.com>
2021-10-03 16:24:46 +0100

    Add tasks routes to the server

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const cors = require("cors");
+const tasksRoutes = require('./routes/tasks');
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(tasksRoutes);
 
 app.get("/", (req, res) => {
   const app = { name: "task management application" };
