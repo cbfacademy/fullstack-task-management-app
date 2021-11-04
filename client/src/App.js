027e2b78357dce7acd02e81355f71e77c10455d6
Adele Kufour <adele.kufour@news.co.uk>
2021-11-04 19:54:25 +0000

    adding a title

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import TaskForm from "./tasks/TaskForm";
 import TasksTable from "./tasks/TasksTable";
+import TaskTile from "./tasks/TaskTile";
 
 function App() {
-  return (
-    <div className="App container">
-      <div>
-        <TaskForm />
-      </div>
-      <div>
-        <TasksTable />
-      </div>
-    </div>
-  );
+	return (
+		<div className="App container">
+			<TaskTile />
+			<div>
+				<TaskForm />
+			</div>
+			<div>
+				<TasksTable />
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ client/src/tasks/TaskTile.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const TaskTile = () => {
+	return (
+		<div className="App-header">
+			<h1>Task Management App</h1>
+		</div>
+	);
+};
+
+export default TaskTile;
